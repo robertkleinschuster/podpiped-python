32b687f6f4271f6264d0164bc9c1f3dfde63a42f
robertkleinschuster <github@robs.social>
2024-07-08 19:10:14 +0200

    refactor podcast builder

--- podpiped/podcast_builder.py
+++ podpiped/podcast_builder.py
@@ -9,25 +9,25 @@ class PodcastBuilderException(Exception):
 
 class PodcastBuilder:
     def __init__(self):
-        self._channel: Channel | None = None
-        self._streams: List[Stream] = []
+        self.__channel: Channel | None = None
+        self.__streams: List[Stream] = []
 
     def set_channel(self, channel: Channel):
-        self._channel = channel
+        self.__channel = channel
 
     def add_stream(self, stream: Stream):
-        self._streams.append(stream)
+        self.__streams.append(stream)
         return self
 
     @property
     def episode_count(self) -> int:
-        return len(self._streams)
+        return len(self.__streams)
 
     def build(self) -> Podcast:
-        if self._channel == None:
+        if self.__channel == None:
             raise PodcastBuilderException("Channel must be set")
 
-        if len(self._streams) == 0:
+        if len(self.__streams) == 0:
             raise PodcastBuilderException("Stream must be set")
 
         episodes: List[Episode] = list(map(lambda stream: Episode(
@@ -39,14 +39,14 @@ class PodcastBuilder:
                 lambda video_stream: video_stream.videoOnly == False and video_stream.mimeType == 'video/mp4',
                 stream.videoStreams
             ))[0].url
-        ), self._streams))
+        ), self.__streams))
 
         return Podcast(
-            id=self._channel.id,
-            title=self._channel.name,
-            description=str(self._channel.description),
-            image=self._channel.avatarUrl,
-            author=self._channel.name,
-            link=f"https://piped.video/channel/{self._channel.id}",
+            id=self.__channel.id,
+            title=self.__channel.name,
+            description=str(self.__channel.description),
+            image=self.__channel.avatarUrl,
+            author=self.__channel.name,
+            link=f"https://piped.video/channel/{self.__channel.id}",
             episodes=episodes
         )
